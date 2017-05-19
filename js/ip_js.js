b4f6414eaa9cbe15dcb18220f48203cba1a70388
yangzhuojia <myangzhuojia@gmail.com>
2017-05-19 09:19:19 +0800

    add sort

--- js/ip_js.js
+++ js/ip_js.js
@@ -5,7 +5,7 @@ window.onload=function(){
     new Vue({
         el: '#box',
         data: {
-            mydata: [["117.135.198.11", 80, 5], ["47.89.49.178", 3128, 9]]
+            mydata: [["117.135.198.11", 80, 10], ["47.89.49.178", 3128, 8],["47.89.49.178", 3128, 9],["117.135.198.10", 80, 10]].sort(function(a,b){return b[2]-a[2]})
         },
         mounted: function () {
             this.$http.jsonp('http://ip.api.yangzhuojia.com/', {}, {
