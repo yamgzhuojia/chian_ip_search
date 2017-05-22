89fa8716d64e7d373cd2324fabd03da556dd02a2
yangzhuojia <myangzhuojia@gmail.com>
2017-05-22 11:37:18 +0800

    init

--- js/ip_js.js
+++ js/ip_js.js
@@ -5,12 +5,10 @@ window.onload=function(){
     new Vue({
         el: '#box',
         data: {
-            mydata: [["117.135.198.11", 80, 10], ["47.89.49.178", 3128, 8],["47.89.49.178", 3128, 9],["117.135.198.10", 80, 10]].sort(function(a,b){return b[2]-a[2]})
+            mydata: [].sort(function(a,b){return b[2]-a[2]})
         },
         mounted: function () {
-            this.$http.jsonp('http://ip.api.yangzhuojia.com/', {}, {
-                jsonp: 'callback'
-            }).then(function (res) {
+            this.$http.get('https://ip.yangzhuojia.com/ip', {},).then(function (res) {
                 this.mydata = res.data
             }, function () {
                 console.log("failed")
