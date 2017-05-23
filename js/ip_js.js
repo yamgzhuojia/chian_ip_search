3e389ca57b798f2f89820d35d9c8e76dbad13ffc
yangzhuojia <myangzhuojia@gmail.com>
2017-05-23 09:08:53 +0800

    add res sort

--- js/ip_js.js
+++ js/ip_js.js
@@ -5,11 +5,11 @@ window.onload=function(){
     new Vue({
         el: '#box',
         data: {
-            mydata: [].sort(function(a,b){return b[2]-a[2]})
+            mydata: []
         },
         mounted: function () {
             this.$http.get('https://ip.yangzhuojia.com/ip', {}).then(function(res){
-                this.mydata = res.data
+                this.mydata = res.data.sort(function(a,b){return b[2]-a[2]})
             },function () {
                 console.log("failed")
             })
